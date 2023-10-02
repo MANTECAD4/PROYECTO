94cbb3fecddb39b786458d357b2aa0203ad8ea55
Daniel Martínez <daniel.martinez5659@alumnos.udg.mx>
2023-10-01 23:20:32 -0600

    Se adapto index para ser totalmente responsive

--- resources/views/Producto/indexProducto.blade.php
+++ resources/views/Producto/indexProducto.blade.php
@@ -46,51 +46,54 @@
                 <h3 class="card-title">Datatables</h3>
                 <p>Información de todos los productos registrados en la tienda. Haz click sobre el nombre un producto para inspeccionarlo!</p>
                 <!-- Table with stripped rows -->
-                <table class="table datatable">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">NOMBRE</th>
-                      <th scope="col">PRECIO</th>
-                      <th scope="col">UNIDADES</th>
-                      <th scope="col">MARCA</th>
-                      <th scope="col">CATEGORÍA</th>
-                      <th scope="col">CREATED_AT</th>
-                      <th scope="col">UPDATED_AT</th>
-                      <th scope="col"></th>
-                      <th scope="col"></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach ($productos as $producto)   
+                <div class="tabla-contenedor">
+                  <table class="table datatable">
+                    <thead>
                       <tr>
-                          <td scope="row">{{ $producto->id }}</td>
-                          <td>
-                              <a href="{{route('producto.show',$producto)}}" title="Inspeccionar producto">
-                                  {{ $producto->nombre }}
-                              </a>
-                              <small class="d-block">{{ $producto->descripcion }}</small>
-                          </td>
-                          <td>${{ $producto->precio }}</td>
-                          <td>{{ $producto->unidades }}</td>
-                          <td>{{ $producto->marca }}</td>                        
-                          <td>{{ $producto->categoria }}</td>
-                          <td>{{ $producto->created_at }}</td>
-                          <td>{{ $producto->updated_at }}</td>
-                          <td><a href="{{route('producto.edit',$producto)}} " class="btn btn-warning" title="Editar producto"><span class="bi bi-pencil"></span></a></td>
-                          <td>
-                            <form action="{{route('producto.destroy',$producto)}}" method="POST">
-                              @csrf
-                              @method('DELETE')
-                              <button type="submit" class="btn btn-danger" title="Borrar producto">
-                                <span class="bi bi-trash"></span>
-                              </button>
-                            </form>
-                          </td>
+                        <th scope="col">#</th>
+                        <th scope="col">NOMBRE</th>
+                        <th scope="col">PRECIO</th>
+                        <th scope="col">UNIDADES</th>
+                        <th scope="col">MARCA</th>
+                        <th scope="col">CATEGORÍA</th>
+                        <th scope="col">CREATED_AT</th>
+                        <th scope="col">UPDATED_AT</th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
                       </tr>
-                    @endforeach
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody>
+                      @foreach ($productos as $producto)   
+                        <tr>
+                            <td scope="row">{{ $producto->id }}</td>
+                            <td>
+                                <a href="{{route('producto.show',$producto)}}" title="Inspeccionar producto">
+                                    {{ $producto->nombre }}
+                                </a>
+                                <small class="d-block">{{ $producto->descripcion }}</small>
+                            </td>
+                            <td>${{ $producto->precio }}</td>
+                            <td>{{ $producto->unidades }}</td>
+                            <td>{{ $producto->marca }}</td>                        
+                            <td>{{ $producto->categoria }}</td>
+                            <td>{{ $producto->created_at }}</td>
+                            <td>{{ $producto->updated_at }}</td>
+                            <td><a href="{{route('producto.edit',$producto)}} " class="btn btn-warning" title="Editar producto"><span class="bi bi-pencil"></span></a></td>
+                            <td>
+                              <form action="{{route('producto.destroy',$producto)}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger" title="Borrar producto">
+                                  <span class="bi bi-trash"></span>
+                                </button>
+                              </form>
+                            </td>
+                        </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+                  
                 <!-- End Table with stripped rows -->
                 <div class="text-center">
                   <a type="button" href="{{route('producto.create')}}" class="btn btn-outline-primary mx-auto"><i class="bi bi-plus-square me-2"></i>Dar de alta producto</a>
--- resources/views/navBar.blade.php
+++ resources/views/navBar.blade.php
@@ -87,52 +87,46 @@
 
 <!-- ======= Sidebar ======= -->
 <aside id="sidebar" class="sidebar">
-
-<ul class="sidebar-nav" id="sidebar-nav">
-
-
-    <li class="nav-item">
-    <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-tag"></i><span>Productos</span><i class="bi bi-chevron-down ms-auto"></i>
-    </a>
-    <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-        <a href="{{route('producto.index')}}">
-            <i class="bi bi-circle"></i><span>Ver productos</span>
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-tag"></i><span>Productos</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
-        </li>
-        <li>
-        <a href="{{route('producto.create')}}">
-            <i class="bi bi-circle"></i><span>Dar de alta productos</span>
+        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <li>
+            <a href="{{route('producto.index')}}">
+                <i class="bi bi-circle"></i><span>Ver productos</span>
+            </a>
+            </li>
+            <li>
+            <a href="{{route('producto.create')}}">
+                <i class="bi bi-circle"></i><span>Dar de alta productos</span>
+            </a>
+            </li>
+        </ul>
+        </li><!-- End Productos Nav -->
+
+        <li class="nav-heading">Pages</li>
+
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+            <i class="bi bi-person"></i>
+            <span>Perfil</span>
         </a>
-        </li>
+        </li><!-- End Profile Page Nav -->
+
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+            <i class="bi bi-box-arrow-in-right"></i>
+            <span>Login</span>
+        </a>
+        </li><!-- End Login Page Nav -->
+
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+            <i class="bi bi-dash-circle"></i>
+            <span>Error 404</span>
+        </a>
+        </li><!-- End Error 404 Page Nav -->
     </ul>
-    </li><!-- End Productos Nav -->
-
-    <li class="nav-heading">Pages</li>
-
-    <li class="nav-item">
-    <a class="nav-link collapsed" href="#">
-        <i class="bi bi-person"></i>
-        <span>Perfil</span>
-    </a>
-    </li><!-- End Profile Page Nav -->
-
-    <li class="nav-item">
-    <a class="nav-link collapsed" href="#">
-        <i class="bi bi-box-arrow-in-right"></i>
-        <span>Login</span>
-    </a>
-    </li><!-- End Login Page Nav -->
-
-    <li class="nav-item">
-    <a class="nav-link collapsed" href="#">
-        <i class="bi bi-dash-circle"></i>
-        <span>Error 404</span>
-    </a>
-    </li><!-- End Error 404 Page Nav -->
-
-
-</ul>
-
 </aside><!-- End Sidebar-->
